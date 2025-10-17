4038d0fbb49eabde49eca8a35ab3079607c2ca92
jesstran5 <72947272+jesstran5@users.noreply.github.com>
2025-10-16 20:49:31 -0600

    Updated cobe_functions from lab 7

--- cobe_functions.py
+++ cobe_functions.py
@@ -1,4 +1,6 @@
 import xarray as xr
+import numpy as np
+from math import pi
 
 '''
 Adapted from `Lab4-modules.ipynb` file of given code in the cells
@@ -16,5 +18,24 @@ def get_time_series_at_coordinate_latest_sst(latitude, longitude, dataset_path):
 
     return time, sst[:, latitude, longitude]
 
+def get_times_from_dataset(sst_ds):
+    return sst_ds['time']
+
 def load_time():
     return
+
+def read_entire_ds(dataset_path):
+    sst_ds = xr.load_dataset(dataset_path)
+
+    return sst_ds
+
+def get_velocity_slope_from_dataset_coordinate(sst_np, G_months, latitude, longitude):
+    sst_np_coordinate = sst_np[:, latitude, longitude]
+
+    # Find solution vector parameters m
+    m_months = np.linalg.inv(G_months.T @ G_months) @ G_months.T @ sst_np_coordinate
+
+    return m_months[1]
+
+
+
